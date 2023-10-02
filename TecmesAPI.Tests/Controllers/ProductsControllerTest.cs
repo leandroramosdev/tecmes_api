a6c5d58c83ce24d4e3c78af519e775b29ed6aa94
Leandro Ramos <leanduardo@hotmail.com>
2023-10-02 18:50:16 -0300

    feat: Added Unit tests

---
+++ TecmesAPI.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using TecmesAPI.Models;
+using TecmesAPI.Services.Interfaces;
+
+namespace TecmesAPI.Tests.Controllers;
+
+public class ProductsControllerTest
+{
+    private readonly IProductService _productService;
+
+    public ProductsControllerTest()
+    {
+        _productService = A.Fake<IProductService>();
+    }
+
+    [TestCase]
+    public void getAllProducts()
+    {
+        var products = A.Fake<List<Product>>();
+        A.CallTo(() => _productService.getAllProducts()).Returns(products);
+        Assert.NotNull(products);
+    }
+
+    [Test]
+    public void addProduct()
+    {
+        var product = A.Fake<Product>();
+        product.Name = "Produto Teste";
+        A.CallTo(() => _productService.addProduct(product)).Returns(product);
+
+        Assert.NotNull(product.Id);
+        Assert.IsInstanceOf<Product>(product);
+    }
+}
+
